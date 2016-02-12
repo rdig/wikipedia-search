8001ee2578437e01e9dd2cc7e3e131382ca43cf6
Raul Glogovetan <raul@glogovetan.com>
2016-02-12 10:33:17 +0200

    api location generator

--- sources/scripts/boilerplate.js
+++ sources/scripts/boilerplate.js
@@ -1,4 +1,14 @@
-const hooks = {
+const Api = (search) => {
+	return (
+		'http://en.wikipedia.org/w/api.php' + '?' +
+		'action=opensearch' + '&' +
+		'format=json' + '&' +
+		'search=' + search + '&' +
+		'limit=20' + '&' +
+		'callback=?'
+	);
+};
+const Hooks = {
 	body: $('body'),
 	lucky: $('#random'),
 	loading: $('.loading'),
